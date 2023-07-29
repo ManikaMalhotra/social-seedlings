99724e6368e3616cf3ca2a18845effcf94d352f2
ManikaMalhotra <alice.ryan36912@gmail.com>
2023-07-30 03:07:53 +0530

    edit profile icon removed

--- src/components/UserInformation/ProfileInfoContainer/index.tsx
+++ src/components/UserInformation/ProfileInfoContainer/index.tsx
@@ -4,15 +4,11 @@ import styles from './styles.module.css';
 export default function ProfileInfoContainer({ user }: IProfileInfoContainerProps) {
     return (
         <div className={styles.profileInfo592Container}>
-            <div className={styles.profileInfo592UserSettings}>
-            <div className={styles.profileInfo592Img}>
-                <img src={user.profile_image.large} alt="" className={styles.profileInfo592Img} />
-            </div>
+            <div className={styles.profileInfo592UserPicName}>
+                <div className={styles.profileInfo592Img}>
+                    <img src={user.profile_image.large} alt="" className={styles.profileInfo592Img} />
+                </div>
                 <h1 className={styles.profileInfo592Username}>{user.username}</h1>
-                <button className={`${styles.profileInfo592Button} ${styles.profileInfo592EditProfileBtn}`}>Edit Profile</button>
-                <button className={`${styles.profileInfo592Button} ${styles.profileInfo592SettingsIcon}`} aria-label="profile settings">
-                    <i className="fas fa-cog" aria-hidden="true"></i>
-                </button>
             </div>
 
             <div className={styles.profileInfo592Stats}>

f76a8d5c02d145b40f869824ce977723e1e68fce
ManikaMalhotra <alice.ryan36912@gmail.com>
2023-07-30 04:09:17 +0530

    dark mode css colors

--- src/components/Layouts/Base/index.tsx
+++ src/components/Layouts/Base/index.tsx
@@ -1,11 +1,12 @@
 import Header from "@/components/Header";
+import styles from "@/styles/BaseLayout.module.css";
 import { IBaseLayoutProps } from "@/types/props";
 
 export default function BaseLayout({ children }: IBaseLayoutProps) {
 	return (
 		<div>
             <Header />
-			<main>{children}</main>
+			<main className={styles.base734Main}>{children}</main>
 		</div>
 	);
 }
